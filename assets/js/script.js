82408b9348c00499f67c169b2682e89d67f1fa87
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-28 11:55:57 +0800

    added sound on mousemove

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+let wave, playing, freq, amp
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
+  wave = new p5.Oscillator('sine')  
 }
 
 function windowResized() {
@@ -7,5 +10,27 @@ function windowResized() {
 }
 
 function draw() {
-  console.log("test")
+  if (playing) {
+    wave.freq(freq, 0.1)
+    wave.amp(amp, 0.1)
+  }
+}
+
+function mousePressed() {
+  playing = playing ? false : true
+  if(playing) {
+    wave.start()
+  } else {
+    wave.stop()
+  }
+}
+
+function mouseMoved() {
+  if(playing) {
+    freq = constrain(map(mouseX, 0, width, 100, 500), 100, height)
+    amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1)
+    fill(0)
+    noStroke()
+    rect(mouseX, mouseY, 10, 10)
+  }
 }
